748a59337f8c713b494195f668c04e8fdceb2b13
RachelBirdy <rachel@weird.tf>
2025-06-22 10:39:53 +0100

    Send midi note off to different function

--- libntxm/arm9/source/fifocommand9.cpp
+++ libntxm/arm9/source/fifocommand9.cpp
@@ -205,6 +205,18 @@ void CommandStopInst(u8 channel)
     fifoSendDatamsg(FIFO_NTXM, sizeof(command), (u8*)&command);
 }
 
+void CommandStopMidiInst(u8 inst, u8 note, u8 volume, u8 channel)
+{
+    NTXMFifoMessage command;
+    command.commandType = STOP_INST;
+
+    StopInstCommand* c = &command.stopInst;
+
+    c->channel = channel;
+
+    fifoSendDatamsg(FIFO_NTXM, sizeof(command), (u8*)&command);
+}
+
 void CommandMicOn(void)
 {
     NTXMFifoMessage command;
