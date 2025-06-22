28e0f3b7f969af20abee4426b5c6606e86bd5c8e
RachelBirdy <rachel@weird.tf>
2025-06-22 11:03:31 +0100

    Send midi stop to new command

--- libntxm/arm9/source/fifocommand9.cpp
+++ libntxm/arm9/source/fifocommand9.cpp
@@ -208,11 +208,14 @@ void CommandStopInst(u8 channel)
 void CommandStopMidiInst(u8 inst, u8 note, u8 volume, u8 channel)
 {
     NTXMFifoMessage command;
-    command.commandType = STOP_INST;
+    command.commandType = STOP_MIDI_INST;
 
-    StopInstCommand* c = &command.stopInst;
+    StopMidiInstCommand* c = &command.stopMidiInst;
 
     c->channel = channel;
+    c->note = note;
+    c->volume = volume;
+    c->inst = inst;
 
     fifoSendDatamsg(FIFO_NTXM, sizeof(command), (u8*)&command);
 }
