bec4159fabe2e367580b2dfd6c0e42d28baf1f76
Adrian Siekierka <kontakt@asie.pl>
2026-03-21 13:44:49 +0100

    fix accessing uninitialized memory when playing auto notes

--- libntxm/arm7/source/player.cpp
+++ libntxm/arm7/source/player.cpp
@@ -205,12 +205,14 @@ void Player::playNote(u8 note, u8 volume, u8 channel, u8 instidx)
 	smp->setPanning(pan);
 
 	// special case: check for sample offset
-	u8 effect = song->patterns[state.pattern][channel][state.row].effect;
-	u8 param  = song->patterns[state.pattern][channel][state.row].effect_param;
-
 	u8 offs = 0;
-	if (effect == EFFECT_SAMPLE_OFFSET)
-		offs = param;
+	if (channel < song->getChannels()) {
+		u8 effect = song->patterns[state.pattern][channel][state.row].effect;
+		u8 param  = song->patterns[state.pattern][channel][state.row].effect_param;
+
+		if (effect == EFFECT_SAMPLE_OFFSET)
+			offs = param;
+	}
 	
 	inst->play(note, volume, channel, offs);
 }
